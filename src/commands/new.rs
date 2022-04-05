41b4721a48e25c83ae3dfc91e3af75d38e53f73e
Nebula <infinitewormhole@gmail.com>
2022-04-05 23:46:13 +0000

    c++ and unused result

--- src/commands/new.rs
+++ src/commands/new.rs
@@ -3,6 +3,6 @@ use std::{fs, io, path::Path};
 pub fn new(name: &str, c: bool) -> io::Result<()> {
     let path = Path::new(name);
     fs::create_dir(path)?;
-    super::init::init(path, c);
+    super::init::init(path, c)?;
     Ok(())
 }
